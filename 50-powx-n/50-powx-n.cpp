ed3d775a3a1487e6cbb7e54e2482f246dd72ef29
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2022-09-24 23:54:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (81.49%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -5,7 +5,7 @@ public:
         if(n==0) return 1;
         temp = calculatePow(x, n/2);
         if(n%2==0) return temp*temp;   
-        else return x*temp*temp;      
+        else return x*temp*temp;    
     }
     
     double myPow(double x, int n) {
