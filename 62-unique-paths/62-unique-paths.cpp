a4a213a6819908f9f86a94caf37d68d99a4d6319
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2022-09-28 00:27:16 +0530

    Time: 2 ms (50.39%), Space: 6 MB (75.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,23 @@
 class Solution {
-public:    
-    int uniquePaths(int m, int n) {
-        int dp[m][n];
-        for(int i=0;i<m;i++){
-            dp[i][n-1] = 1;
+public:
+    int dp[101][101];
+    
+    int findPath(int m, int n){
+        if(m==0 && n==0){
+            return 1;
         }
-        for(int i=0;i<n;i++){
-            dp[m-1][i] = 1;
-        }
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
-            }
-        }
-        return dp[0][0];
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        int totalWays = 0;
+        if(m!=0) totalWays += findPath(m-1,n);
+        if(n!=0) totalWays += findPath(m,n-1);
+        return dp[m][n] = totalWays;
+    }
+    
+    int uniquePaths(int m, int n){
+        for(int i=0;i<101;i++)
+            for(int j=0;j<101;j++)
+                dp[i][j] = -1;
+        return findPath(m-1,n-1);
     }
 };
