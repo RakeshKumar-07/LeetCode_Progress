b829f60a509699d244f656c844f19b57019a41e4
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2022-09-29 20:00:18 +0530

    Time: 142 ms (32.79%), Space: 31.9 MB (52.53%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        if(k==n){
+            return arr;
+        }
+        vector<int> ans;
+        arr.push_back(x);
+        int e = 0;
+        sort(arr.begin(), arr.end());
+        int p,q;
+        for(int i=0;i<n+1;i++){
+            if(arr[i] == x){
+                p = i;
+                q = i;
+                break;
+            }
+        }
+        // cout<<p<<" "<<q<<endl;
+        if(p==0){
+            for(int i=0;i<k;i++){
+                q++;
+                ans.push_back(arr[q]);
+            }
+            sort(ans.begin(),ans.end());
+            return ans;
+        }
+        if(q==n){
+            for(int i=0;i<k;i++){
+                p--;
+                ans.push_back(arr[p]);
+            }
+            sort(ans.begin(),ans.end());
+            return ans;
+        }
+        p--,q++;
+        // cout<<"Hi"<<endl;
+        for(int i=0;i<k;i++){
+            if( q<=n && p>=0 && abs(arr[p]-x) > abs(arr[q]-x) || p<0){
+                ans.push_back(arr[q]);  
+                q++;
+                e++;
+            }
+            else if(p>=0 && q<=n && abs(arr[p]-x) <= abs(arr[q]-x) || q>n){
+                ans.push_back(arr[p]);
+                p--;
+                e++;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
