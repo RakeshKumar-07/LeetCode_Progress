087fddbb732c9053bbe441f9cee248d114c2eb3e
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2022-09-29 23:45:04 +0530

    Time: 114 ms (48.27%), Space: 32.5 MB (50.92%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
+        deque<int> dq;
         vector<int> ans;
         int p,q;
         for(int i=0;i<n;i++){
@@ -13,15 +14,24 @@ public:
         p = q-1;
         for(int i=0;i<k;i++){
             if( q<=n-1 && p>=0 && abs(arr[p]-x) > abs(arr[q]-x) || p<0){
-                ans.push_back(arr[q]);  
+                dq.push_back(arr[q]);  
                 q++;
             }
             else if(p>=0 && q<=n-1 && abs(arr[p]-x) <= abs(arr[q]-x) || q>n-1){
-                ans.push_back(arr[p]);
+                dq.push_front(arr[p]);
                 p--;
             }
         }
-        sort(ans.begin(),ans.end());
+        for(int i=0;i<k;i++){
+            if(dq.front() <= dq.back()){
+                ans.push_back(dq.front());
+                dq.pop_front();
+            }
+            else{
+                ans.push_back(dq.back());
+                dq.pop_back();
+            }
+        }
         return ans;
     }
 };
