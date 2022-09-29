2bcd8d5e1b92c58a3a04cabb71a5066592c2bd94
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2022-09-29 23:13:56 +0530

    Time: 136 ms (35.05%), Space: 31.8 MB (61.64%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -7,7 +7,7 @@ public:
         sort(arr.begin(), arr.end());
         int p,q;
         for(int i=0;i<n+1;i++){
-            if(arr[i] == x){
+            if(arr[i] >= x){
                 p = i;
                 q = i;
                 break;
