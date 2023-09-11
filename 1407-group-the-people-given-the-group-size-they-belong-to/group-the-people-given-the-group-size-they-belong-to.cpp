7c6e6194e05b6d1c8d6be82e3082fd7aa57b8e9e
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-09-11 13:41:57 +0530

    Time: 14 ms (35.21%) | Memory: 13.8 MB (24.77%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        map<int, vector<int>> m;
+        unordered_map<int, vector<int>> m;
         for(int i=0;i<groupSizes.size();i++){
             m[groupSizes[i]].push_back(i);
         }
