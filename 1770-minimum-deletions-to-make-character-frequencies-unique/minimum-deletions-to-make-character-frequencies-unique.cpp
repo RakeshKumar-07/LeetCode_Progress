4615e45411d248752b2edfb765cde193ede486c2
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-09-12 11:59:17 +0530

    Time: 292 ms (5.08%) | Memory: 131 MB (5.62%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        // cout<<s.size()<<endl;
         sort(s.begin(), s.end());
         map<int, vector<char>, greater<int>> m;
         int cnt = 1, maxi = 0;
@@ -19,18 +18,6 @@ public:
         for(auto i:m) vis[i.first] = true;
         vector<int> v;
         for(int i=0;i<vis.size();i++) if(!vis[i]) v.push_back(i);
-
-        // for(auto i:m){
-        //     cout<<i.first<<" -> ";
-        //     for(int j=0;j<i.second.size();j++){
-        //         cout<<i.second[j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-
-        // for(int i=0;i<v.size();i++){
-        //     cout<<v[i]<<endl;
-        // }
 
         int k = v.size()-1;
         int ans = 0;
