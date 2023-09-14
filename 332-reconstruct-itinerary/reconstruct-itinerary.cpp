2f7ce28915e7a1034b6fee8c8ab2dd1022218a14
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-09-14 23:17:12 +0530

    Time: 15 ms (89.66%) | Memory: 14.7 MB (55.27%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
+
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string, vector<string>> graph;
-        
-        for (auto& ticket : tickets) {
-            graph[ticket[0]].push_back(ticket[1]);
+        unordered_map<string, vector<string>> m;
+        for(int i=0;i<tickets.size();i++){
+            m[tickets[i][0]].push_back(tickets[i][1]);
         }
-        
-        for (auto& [_, dests] : graph) {
+
+        for (auto& [_, dests] : m) {
             sort(dests.rbegin(), dests.rend());
         }
-        
-        vector<string> stack = {"JFK"};
-        vector<string> itinerary;
-        
-        while (!stack.empty()) {
-            string curr = stack.back();
-            if (graph.find(curr) != graph.end() && !graph[curr].empty()) {
-                stack.push_back(graph[curr].back());
-                graph[curr].pop_back();
-            } else {
-                itinerary.push_back(stack.back());
-                stack.pop_back();
+
+        vector<string> ans;
+        stack<string> s;
+        s.push("JFK");
+        while(!s.empty()){
+            string t = s.top();
+            if(m.find(t) != m.end() && m[t].size() > 0){
+                s.push(m[t].back());
+                m[t].pop_back();
+            }
+            else{
+                ans.push_back(t);
+                s.pop();
             }
         }
-        
-        reverse(itinerary.begin(), itinerary.end());
-        return itinerary;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
