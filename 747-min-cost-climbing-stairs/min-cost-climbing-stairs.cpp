f095170a1a11c776bb9c3016a652a505f09292b6
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-10-13 14:10:48 +0530

    Time: 3 ms (84.96%) | Memory: 13.9 MB (37.30%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -4,9 +4,7 @@ public:
         int dp[cost.size()];
         dp[0] = cost[0];
         dp[1] = cost[1];
-        for(int i=2;i<cost.size();i++){
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
-        }
+        for(int i=2;i<cost.size();i++) dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
         return min(dp[cost.size()-1], dp[cost.size()-2]);
     }
 };
