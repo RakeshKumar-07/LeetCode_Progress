bb0ca7c2fe9fecdd59cd2c973d10d1b8077fa5ad
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-10-27 23:45:24 +0530

    Time: 3 ms (99.43%) | Memory: 7.3 MB (72.80%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ans = "";
+        pair<int,int> ans = {1,0};
         for(int i=0;i<s.size();i++){
             int p = i-1, q = i+1;
-            string t = "";
-            t += s[i];
+            int size = 1;
             while(p>=0 && q<s.size() && s[p] == s[q]){
-                t = s[p] + t + s[q];
                 p--;
                 q++;
+                size += 2;
             }
-            if(ans.size() < t.size()) ans = t;
+            if((ans.second - ans.first + 1) < (size)) ans = {p+1, q-1};
         }
         for(int i=0;i<s.size()-1;i++){
             if(s[i] == s[i+1]){
                 int p = i-1, q = i+2;
-                string t = "";
-                t += s[i];
-                t += s[i+1];
+                int size = 2;
                 while(p>=0 && q<s.size() && s[p] == s[q]){
-                    t = s[p] + t + s[q];
                     p--;
                     q++;
+                    size += 2;
                 }
-                if(ans.size() < t.size()) ans = t;
+                if((ans.second - ans.first + 1) < (size)) ans = {p+1, q-1};
             }
         }
-        return ans;
+        return s.substr(ans.first, ans.second - ans.first + 1);
     }
 };
