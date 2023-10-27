4fc57e1b6157114b9055309fee9e2304739699c0
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-10-27 19:47:48 +0530

    Time: 2134 ms (5.00%) | Memory: 486.9 MB (6.60%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string ans = "";
+        // cout<<s.size()<<endl;
+        for(int i=0;i<s.size();i++){
+            int p = i-1, q = i+1;
+            string t = "";
+            t += s[i];
+            while(p>=0 && q<s.size() && s[p] == s[q]){
+                t = s[p] + t + s[q];
+                p--;
+                q++;
+            }
+            if(ans.size() < t.size()) ans = t;
+        }
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i] == s[i+1]){
+                int p = i-1, q = i+2;
+                string t = "";
+                t += s[i];
+                t += s[i+1];
+                while(p>=0 && q<s.size() && s[p] == s[q]){
+                    t = s[p] + t + s[q];
+                    p--;
+                    q++;
+                }
+                if(ans.size() < t.size()) ans = t;
+            }
+        }
+        return ans;
+    }
+};
