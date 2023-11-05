fa407bb40339ace7cfc10a33390546cc7515759a
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-11-05 11:43:07 +0530

    Time: 91 ms (44.44%) | Memory: 63.4 MB (86.11%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -11,12 +11,7 @@ public:
         }
         if(k >= arr.size() || v.size() == 1) return maxi;
 
-        for(int i=0;i<v.size()-1;i++){
-            // cout<<(v[i+1].first - v[i].first - 1) + (v[i].first - 1 >= 0 && arr[v[i].first - 1] < v[i].second ? 1 : 0)<<endl;
-            if((v[i+1].first - v[i].first - 1) + (v[i].first - 1 >= 0 && arr[v[i].first - 1] < v[i].second ? 1 : 0) >= k ){
-                return v[i].second;
-            }
-        }
+        for(int i=0;i<v.size()-1;i++) if((v[i+1].first - v[i].first - 1) + (v[i].first - 1 >= 0 && arr[v[i].first - 1] < v[i].second ? 1 : 0) >= k ) return v[i].second;
 
         return maxi;
     }
