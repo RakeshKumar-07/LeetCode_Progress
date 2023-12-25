e9ada451c0e8dc48a0f16f5ada48f2ba601d26fe
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2023-12-25 17:28:37 +0530

    Time: 5 ms (14.96%) | Memory: 7.8 MB (21.10%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[101];
+
+    int solve(int i, int n, string s){
+        if(i==n) return 1;
+        if(dp[i] != -1) return dp[i];
+        int t = 0;
+        if(s[i] != '0') t += solve(i+1, n, s);
+        if(i<n-1 && s[i] != '0' && stoi(s.substr(i,2)) <= 26 && stoi(s.substr(i,2)) >= 10) t+= solve(i+2, n, s);
+        return dp[i] = t;
+    }
+
+    int numDecodings(string s) {
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(0, s.size(), s);
+        return ans;
+    }
+};
