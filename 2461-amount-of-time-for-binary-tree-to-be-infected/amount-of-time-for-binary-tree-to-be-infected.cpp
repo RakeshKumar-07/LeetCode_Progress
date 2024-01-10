6fa603336abb2cb8691941bb64bb8713893c2410
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-01-10 22:30:09 +0530

    Time: 635 ms (16.70%) | Memory: 162.7 MB (37.61%) - LeetSync

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        map<int, vector<int>> m;
+        int ans = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.size() != 0){
+            int size = q.size();
+            while(size--){
+                TreeNode* t = q.front();
+                q.pop();
+                if(t->left != NULL){
+                    m[t->val].push_back(t->left->val);
+                    m[t->left->val].push_back(t->val);
+                    q.push(t->left);
+                }
+                if(t->right != NULL){
+                    m[t->val].push_back(t->right->val);
+                    m[t->right->val].push_back(t->val);
+                    q.push(t->right);
+                }
+            }
+        }
+        vector<bool> vis(100001, false);
+        queue<pair<int, int>> p;
+        p.push({start, 0});
+        while(p.size() != 0){
+            int size = p.size();
+            while(size--){
+                pair<int, int> t = p.front();
+                vis[t.first] = true;
+                ans = max(ans, t.second);
+                p.pop();
+                for(int i = 0;i<m[t.first].size();i++){
+                    if(!vis[m[t.first][i]]){
+                        p.push({m[t.first][i], t.second+1});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
