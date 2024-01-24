a6bdcf0eb9dccdcc950db789fb57ebdbbfc2a4f9
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-01-25 00:48:35 +0530

    Time: 250 ms (85.52%) | Memory: 177.7 MB (98.93%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+
+    void solve(TreeNode* root, vector<int> &f){
+        f[root->val]++;
+        if(root->left == NULL && root->right == NULL){
+            int odd = 0;
+            for(int i=0;i<10;i++) if(f[i]%2 != 0) odd++;
+            if(odd < 2) ans++;
+            f[root->val]--;
+            return;
+        }
+        if(root->left != NULL) solve(root->left, f);
+        if(root->right != NULL) solve(root->right, f);
+        f[root->val]--;
+    }
+
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int> f(10, 0);
+        solve(root, f);
+        return ans;
+    }
+};
