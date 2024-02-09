947d495085ad5ed9d9be08d8498a71ab17a3f000
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-02-09 22:04:31 +0530

    Time: 29 ms (61.46%) | Memory: 15.6 MB (5.01%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans(nums.size());
+        for(int i=0;i<nums.size();i++){
+            for(int j=i;j>=0;j--){
+                if(nums[i]%nums[j] == 0 && ans[j].size() >= ans[i].size()){
+                    vector<int> t = ans[j];
+                    t.push_back(nums[i]);
+                    // cout<<"Hi";
+                    ans[i] = t;
+                }
+            }
+        }
+        vector<int> res;
+        for(int i=0;i<ans.size();i++) if(res.size() < ans[i].size()) res = ans[i];
+        return res;
+    }
+};
