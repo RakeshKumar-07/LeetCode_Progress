dd02bad87c7a819497ed5e95204593d56bd78342
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-03-16 16:28:43 +0530

    Time: 320 ms (5.04%) | Memory: 306.4 MB (5.19%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -7,7 +7,6 @@ public:
             if(nums[i] == 1) ones++;
             else zeroes++;
             if(ones == zeroes) ans = max(i+1, ans);
-            // cout<<ones<<" , "<<zeroes<<" => "<<f2[ones-zeroes + 100000]<<" -> "<<f1[zeroes - ones + 100000]<<endl;
             if(f2[ones-zeroes + 100000] != INT_MAX) ans = max(i - f2[ones-zeroes + 100000], ans);
             f1[zeroes - ones + 100000] = min(f1[zeroes - ones + 100000], i);
             f2[ones - zeroes + 100000] = min(f2[ones - zeroes + 100000], i);
