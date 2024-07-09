028e913bf5fe3cbfa0325082a5fa64abb920c700
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-07-09 21:19:53 +0530

    Time: 135 ms (91.76%) | Memory: 76.6 MB (57.65%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -3,8 +3,7 @@ public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double currentTime = 1, waitingTime = 0;
         for(int i=0;i<customers.size();i++){
-            currentTime = max(currentTime, (double)customers[i][0]);
-            currentTime += customers[i][1];
+            currentTime = max(currentTime, (double)customers[i][0]) + customers[i][1];
             waitingTime += (currentTime - customers[i][0]);
         }
         return waitingTime/customers.size();
