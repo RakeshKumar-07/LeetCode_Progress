4448e15dd6b0bc34594e8d14752394e6d011a607
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-07-12 15:58:02 +0530

    Time: 455 ms (5.29%) | Memory: 382.4 MB (5.28%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        vector<string> vec;
+        string str = "";
+        for(int i=0;i<s.size();i++){
+            if(s[i] == 'a' || s[i] == 'b') str += s[i];
+            else{
+                if(str.size() != 0) vec.push_back(str);
+                str = "";
+            }
+        }
+        if(str.size() != 0) vec.push_back(str);
+        // for(int i=0;i<vec.size();i++) cout<<vec[i]<<endl;
+        int ans = 0;
+        if(x > y){
+            // cout<<"X: \n";
+            for(int i=0;i<vec.size();i++){
+                stack<char> st, st1;
+                for(int j=0;j<vec[i].size();j++){
+                    if(st.size() == 0) st.push(vec[i][j]);
+                    else if(st.top() == 'a' && vec[i][j] == 'b'){
+                        st.pop();
+                        ans += x;
+                        // cout<<i<<" "<<j<<" => "<<ans<<endl;
+                    }
+                    else st.push(vec[i][j]);
+                }
+                while(st.size() > 0){
+                    if(st1.size() == 0) st1.push(st.top());
+                    else if(st1.top() == 'a' && st.top() == 'b'){
+                        st1.pop();
+                        ans += y;
+                    }
+                    else st1.push(st.top());
+                    st.pop();
+                }
+            }
+        }
+        else{
+            // cout<<"Y: \n";
+            for(int i=0;i<vec.size();i++){
+                stack<char> st, st1;
+                for(int j=0;j<vec[i].size();j++){
+                    if(st.size() == 0) st.push(vec[i][j]);
+                    else if(st.top() == 'b' && vec[i][j] == 'a'){
+                        st.pop();
+                        ans += y;
+                        // cout<<i<<" "<<j<<" => "<<ans<<endl;
+                    }
+                    else st.push(vec[i][j]);
+                }
+                while(st.size() > 0){
+                    if(st1.size() == 0) st1.push(st.top());
+                    else if(st1.top() == 'b' && st.top() == 'a'){
+                        st1.pop();
+                        ans += x;
+                    }
+                    else st1.push(st.top());
+                    st.pop();
+                }
+            }
+        }
+        return ans;
+    }
+};
