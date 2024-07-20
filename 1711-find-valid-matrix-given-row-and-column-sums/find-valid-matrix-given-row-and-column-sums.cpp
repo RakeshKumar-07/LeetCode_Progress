3778375c1784cd300cb6a77d9ca7e45ed57511c6
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-07-20 14:20:13 +0530

    Time: 47 ms (10.16%) | Memory: 36.2 MB (6.09%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -2,22 +2,12 @@ class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
         vector<vector<int>> v(rowSum.size(), vector<int>(colSum.size(), 0));
-        vector<int> rowCurSum(rowSum.size(), 0), colCurSum(colSum.size(), 0);
-        for(int i=0;i<colSum.size();i++){
-            if(i<colSum.size() && i<rowSum.size()){
-                v[i][i] = min(rowSum[i], colSum[i]);
-                rowCurSum[i] = v[i][i];
-                colCurSum[i] = v[i][i];
-            }
-        }
         for(int i=0;i<rowSum.size();i++){
             for(int j=0;j<colSum.size();j++){
-                if(i != j && rowCurSum[i] < rowSum[i] && colCurSum[j] < colSum[j]){
-                    int minDiff = min(rowSum[i] - rowCurSum[i], colSum[j] - colCurSum[j]);
-                    v[i][j] = minDiff;
-                    rowCurSum[i] += minDiff;
-                    colCurSum[j] += minDiff;
-                }
+                int minDiff = min(rowSum[i], colSum[j]);
+                v[i][j] = minDiff;
+                rowSum[i] -= minDiff;
+                colSum[j] -= minDiff;
             }
         }
         return v;
