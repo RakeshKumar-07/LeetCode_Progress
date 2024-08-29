d32bfb27a795b9d5c283a080bd789e60f43ac6f5
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-08-29 12:21:02 +0530

    Time: 168 ms (9.73%) | Memory: 18.7 MB (77.31%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int parent[1001];
+
+    int find(int i) { 
+        if (parent[i] == i) return i;
+        else return parent[i] = find(parent[i]);
+    } 
+
+    void Union(int i, int j) { 
+        int irep = find(i);
+        int jrep = find(j);
+        if(irep <= jrep) parent[jrep] = irep;
+        else parent[irep] = jrep;
+    }
+
+    int removeStones(vector<vector<int>>& stones) {
+        for(int i=0;i<1001;i++) parent[i] = i;
+
+        for(int i=0;i<stones.size();i++){
+            for(int j=i+1;j<stones.size();j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) Union(i, j);
+            }
+        }
+
+        for(int i=0;i<stones.size();i++){
+            for(int j=i+1;j<stones.size();j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) Union(i, j);
+            }
+        }
+
+        set<int> s;
+
+        for(int i=0;i<stones.size();i++) s.insert(parent[i]);
+
+        for(int i=0;i<stones.size();i++) cout<<parent[i]<<endl;
+
+        return stones.size() - s.size();
+    }
+
+};
