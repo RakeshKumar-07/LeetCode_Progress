64232d0c6a1e39162002d3b4c747cfca0df16ccf
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-09-04 17:02:43 +0530

    Time: 108 ms (34.25%) | Memory: 50.2 MB (24.03%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        bool isY = true, isPos = true;
+        long long x = 0, y = 0, ans = INT_MIN;
+        map<pair<int,int>, bool> m;
+        for(int i=0;i<obstacles.size();i++) m[{obstacles[i][0], obstacles[i][1]}] = true;
+        for(int i=0;i<commands.size();i++){
+            
+            // cout<<x<<" : "<<y<<" => ";
+
+            if(commands[i] >= 1){
+                int tx = 0, ty = 0;
+                // cout<<endl<<"============="<<endl;
+                for(int j=1;j<=commands[i];j++){
+                    // cout<<isY<<" : "<<isPos<<" : "<<j<<endl;
+                    if(isY && isPos && !m[{x, y + j}]) ty = j;
+                    else if(isY && !isPos && !m[{x, y - j}]) ty = -1*j;
+                    else if(!isY && isPos && !m[{x + j, y}]) tx = j;
+                    else if(!isY && !isPos && !m[{x - j, y}]) tx = -1*j;
+                    else break;
+                }
+                // cout<<"============="<<endl;
+                x += tx;
+                y += ty;
+            }
+            else if(commands[i] == -1){
+                if(isY) isY = !isY;
+                else if(!isY){
+                    isPos = !isPos;
+                    isY = !isY;
+                }
+            }
+            else if(commands[i] == -2){
+                if(isY){
+                    isPos = !isPos;
+                    isY = !isY;
+                }
+                else if(!isY) isY = !isY;
+            }
+
+            // cout<<x<<" : "<<y<<endl;
+
+            ans = max(ans, abs(x)*abs(x) + abs(y)*abs(y));
+        }
+        return ans;
+    }
+};
