219d445866bc1111909653c18d76a8052de71c93
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2024-09-05 11:17:44 +0530

    Time: 86 ms (87.50%) | Memory: 118 MB (45.10%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        vector<int> ans(n, 1);
+        int sum = 0, neededSum = 0;
+        for(int i=0;i<rolls.size();i++) sum += rolls[i];
+        neededSum = mean*(rolls.size() + n) - sum;
+        // cout<<neededSum<<endl;
+        if(neededSum < n || ((double)neededSum/6 > n)) return {};
+        neededSum -= n;
+        for(int i=0;i<n;i++){
+            if(neededSum/6 > 0){
+                ans[i] = 6;
+                neededSum -= 5;
+            }
+            else if(neededSum%6 > 0){
+                ans[i] += neededSum;
+                neededSum = 0;
+            }
+            else break;
+        }
+        return ans;
+    }
+};
