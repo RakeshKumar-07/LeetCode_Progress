cf976b35eac61e992c61ffa3e4c54fe1243c4124
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2025-04-06 09:21:33 +0530

    Time: 23 ms (13.66%) | Memory: 14.5 MB (9.59%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
+
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        vector<int> ans;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans(nums.size());
-        for(int i=0;i<nums.size();i++){
-            for(int j=i;j>=0;j--){
-                if(nums[i]%nums[j] == 0 && ans[j].size() >= ans[i].size()){
-                    vector<int> t = ans[j];
-                    t.push_back(nums[i]);
-                    ans[i] = t;
+        vector<int> dp(nums.size(), 0);
+        vector<vector<int>> l(nums.size());
+        for(int i=nums.size()-1;i>=0;i--){
+            int maxDiv = 0, k = -1;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[j]%nums[i] == 0 && maxDiv <= dp[j]){
+                    maxDiv = dp[j];
+                    k = j;
                 }
             }
+            dp[i] = 1 + maxDiv;
+            if(k != -1){
+                for(int j=0;j<l[k].size();j++) l[i].push_back(l[k][j]);
+            }
+            l[i].push_back(nums[i]);
+            // cout<<nums[i]<<" => "<<dp[i]<<" "<<l[i].size()<<"\n";
+            // for(int j=0;j<l[i].size();j++) cout<<l[i][j]<<" ";
+            // cout<<endl;
         }
-        vector<int> res;
-        for(int i=0;i<ans.size();i++) if(res.size() < ans[i].size()) res = ans[i];
-        return res;
+        for(int i=0;i<nums.size();i++) if(ans.size() < l[i].size()) ans = l[i];
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
