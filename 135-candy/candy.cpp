d324f5aaa65d8a9d2de4c59b65024f016a35201c
RakeshKumar-07 <114108634+RakeshKumar-07@users.noreply.github.com>
2025-06-02 10:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (59.49%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,45 +1,18 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int ans = ratings.size(), mini = INT_MAX;
-        vector<int> rat(ratings.size(), 1);
-        map<int, vector<int>> m;
-        for(int i=0;i<ratings.size();i++){
-            m[ratings[i]].push_back(i);
-            mini = min(mini, ratings[i]);
+    int candy(vector<int>& ratings, int cnt = 0) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        for (int i = 1; i < n; i++) 
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
+            cnt += candies[i - 1];
         }
-        for(auto i:m){
-            if(i.first != mini){
-                for(int j=0;j<i.second.size();j++){
-                    if(i.second[j] > 0 && i.second[j] < ratings.size()-1){
-                        if(ratings[i.second[j]] > ratings[i.second[j]+1] && ratings[i.second[j]] > ratings[i.second[j]-1]){
-                            ans += abs(rat[i.second[j]] - (max(rat[i.second[j]+1], rat[i.second[j]-1]) + 1));
-                            rat[i.second[j]] = max(rat[i.second[j]+1], rat[i.second[j]-1]) + 1;
-                        }
-                        else if(ratings[i.second[j]] > ratings[i.second[j]+1]){
-                            ans += abs(rat[i.second[j]] - (rat[i.second[j]+1] + 1));
-                            rat[i.second[j]] = rat[i.second[j]+1] + 1;
-                        }
-                        else if(ratings[i.second[j]] > ratings[i.second[j]-1]){
-                            ans += abs(rat[i.second[j]] - (rat[i.second[j]-1] + 1));
-                            rat[i.second[j]] = rat[i.second[j]-1] + 1;
-                        }
-                    }
-                    else if(i.second[j] == 0){
-                        if(ratings[i.second[j]] > ratings[i.second[j]+1]){
-                            ans += abs(rat[i.second[j]] - (rat[i.second[j]+1] + 1));
-                            rat[i.second[j]] = rat[i.second[j]+1] + 1;
-                        }
-                    }
-                    else if(i.second[j] == ratings.size() - 1){
-                        if(ratings[i.second[j]] > ratings[i.second[j]-1]){
-                            ans += abs(rat[i.second[j]] - (rat[i.second[j]-1] + 1));
-                            rat[i.second[j]] = rat[i.second[j]-1] + 1;
-                        }
-                    }
-                }
-            }
-        }
-        return ans;
+        return cnt + candies[n - 1];
     }
 };
